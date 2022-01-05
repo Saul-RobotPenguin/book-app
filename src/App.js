ad428c4032b42b3715b093d783bf662e52a2a842
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-01-04 21:18:05 -0500

    initial books

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import Header from './components/Header';
+import Book from './components/Book';
 import Footer from './components/Footer';
 
 function App() {
+
+  const book = [{
+    title: 'The Catcher in the Rye',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/81OthjkJBuL.jpg',
+    description: 'The Catcher in the Rye is a novel by J. D. Salinger, partially published in serial form in 1945–1946 and as a novel in 1951. It was originally intended for adults but is often read by adolescents for its themes of angst, alienation, and as a critique on superficiality in society. It has been translated widely.'
+  },
+  {
+    title: 'The Great Gatsby',
+    image: 'https://m.media-amazon.com/images/I/41XMaCHkrgL.jpg',
+    description: 'The Great Gatsby is a 1925 novel written by American author F. Scott Fitzgerald that follows a cast of characters living in the fictional town of West Egg on prosperous Long Island in the summer of 1922. The story primarily concerns the young and mysterious millionaire Jay Gatsby and his quixotic passion for the beautiful Daisy Buchanan, whom he has never met.'
+  },
+  {
+    title: 'The Lord of the Rings',
+    image: 'https://m.media-amazon.com/images/I/51bJhsl5VmL.jpg',
+    description: 'The Lord of the Rings is a fantasy novel written by English author J. R. R. Tolkien. The story began as a sequel to Tolkien\'s 1937 fantasy novel The Hobbit, but eventually developed into a much larger work. Written in stages between 1937 and 1949, it is one of the best-selling novels in English history, with over 150 million copies sold'
+  }
+  ];
   return (
     <>
       <Header />
 
       {/* Book Component Here */}
+      <Book book={book} />
 
       <Footer />
     </>
